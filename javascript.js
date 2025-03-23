ad10bb0e0f320ecfed9b97d25b488b0011108233
ROS DUL <rosdul@ROSs-MacBook-Pro.local>
2025-03-23 20:13:03 +0900

    updated fn&style

--- javascript.js
+++ javascript.js
@@ -1,45 +1,75 @@
 const storeData = [];
-const token = '-token-';
+const token = '  INPUT TOKEN GITHUP   ';
 const url = 'https://api.github.com/search/repositories?q=';
 const inputRepo = document.getElementById('repoInput');
 const suggestionsDiv = document.getElementById('suggestions');
 const repoListDiv = document.getElementById('repoList');
 
-// Autocomplete នៅពេលវាយ
-inputRepo.addEventListener('input', async function() {
-    const query = this.value;
 
-    if (query.length <= 0) {
-        suggestionsDiv.innerHTML = "";
-        return;
+//រងចាំឈប់វាយបញ្ចូល
+function debounce(fn,delay = 500){
+    let timer;
+    return (...arg)=>{
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(()=>{fn(...arg)},delay);
+    }
+}
+
+// past ការវាយបញ្ចូលទៅកាន fn ថ្មី
+const fetchRepo = debounce( async (query)=>{
+    //ផ្ទុយពីមាន
+    if(!query){
+        suggestionsDiv.innerHTML = ""
+        return 
+    }
+    // ករណីស្វែងរក c program
+    if(query.length< 3 && query.toLowerCase() !== 'c'){
+        suggestionsDiv.innerHTML = ""
+        return 
     }
 
+
     try {
-        const response = await fetch(url + query, {
+        const seachQuery = query.toLowerCase() === 'c'? `language  ${query}`: query;
+
+        // fetch API github
+        const response = await fetch(url + seachQuery, {
             headers: { "Authorization": `token ${token}` }
         });
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
         const data = await response.json();
 
+        //ផ្ទាំងដែលបានស្វែងរក
         suggestionsDiv.innerHTML = "";
         data.items.slice(0, 5).forEach((d) => {
             let suggestion = document.createElement('div');
-            suggestion.textContent = `${d.name}, Owner: ${d.owner.login}`;
-            suggestion.style.padding = "5px";
-            suggestion.style.cursor = 'pointer';
-
+            suggestion.innerHTML = `<span>${d.name}</span>,<span> Owner: ${d.owner.login}</span>`;
+        
+            //បោះទិន្ន័យទៅឲ្យ fn addRepo តាមរយចុចលើផ្ទាំងស្វែងរកនីមួយៗ
             suggestion.addEventListener('click', () => {
                 addRepo(d);
                 inputRepo.value = ""; // សម្អាត input
                 suggestionsDiv.innerHTML = ""; // សម្អាត suggestions
             });
 
-            suggestionsDiv.appendChild(suggestion);
+            //កំណត់ទទេ suggestionsDiv
+            (inputRepo.value === "")?suggestionsDiv.innerHTML = "":  suggestionsDiv.appendChild(suggestion);
+
         });
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+
+})
+
+
+// Autocomplete នៅពេលវាយ
+inputRepo.addEventListener('input', async function() {
+    const query = this.value;
+    fetchRepo(query)
+
 });
+
 
 // បន្ថែម repository ទៅ storeData
 function addRepo(repo) {
@@ -56,7 +86,7 @@ function renderRepo(){
                 <div>Name: ${data.name}</div>
                 <div>
                     <div>Owner: ${data.owner.login}</div>
-                    <div>Star: ${data.stargazers_count}</div>
+                    <div>Stars: ${data.stargazers_count}</div>
                     <button onclick="deleteRepo(${index})">x</button>
                 </div>
             </div>`
@@ -65,6 +95,8 @@ function renderRepo(){
     repoListDiv.innerHTML = doList
     
 }
+
+
 
 function deleteRepo(index){
    storeData.splice(index,1)
